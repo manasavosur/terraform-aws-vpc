f5a96682992a4474247934b6c2209383a6b9672c
Manasa <manasa.vosur@gmail.com>
2026-02-28 15:40:29 +0530

    terrafrom aws vpc

--- main.tf
+++ main.tf
@@ -5,3 +5,27 @@ resource "aws_vpc" "main" {
 
   tags = local.vpc_final_tags
 }
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id # VPC association
+
+  tags = local.igw_final_tags
+}
+
+# Public Subnets
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(
+        local.common_tags,
+        # roboshop-dev-public-us-east-1a
+        {
+            Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
+        },
+        var.public_subnet_tags
+    )
+}
